7450c7d9064047b60d3f24c89218306a8f9b70fc
Goran Udosic <gor181@users.noreply.github.com>
2018-04-27 14:26:21 +0200

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,10 +1,9 @@
-# If bash command fails, build should error out
+# If bash command fails, build should fail with an error.
 set -e
 
-##### Install specific package versions with pip #####
+# Install specific package versions with pip
+# NOTE: matplotlib 1.5.3 onward is NOT currently supported. The image comes installed with matplotlib 1.5.1.
 
-# pip3 install pandas==0.19.1
-# pip3 install tweepy==3.5.0
-# pip3 install bs4==4.4.1
-
-# NOTE: matplotlib 1.5.3 onward is NOT currently supported. The image comes installed with matplotlib 1.5.1
+echo "about to pip3 install geopandas"
+pip3 install geopandas
+echo 'geopandas version is' $(python3 -c 'import geopandas; print(geopandas.__version__)')
